65c55f30e8cfc9225c97cc5661bac9e80937a028
Benjamin Berkei <b.berkei95@gmail.com>
2019-07-14 16:15:54 +0200

    Added random initalization to the buttons text

--- Game/Form1.cs
+++ Game/Form1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows.Forms;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Game
 {
@@ -13,6 +14,39 @@ namespace Game
             InitializeComponent();
             hiragana = new HiraganaCharacters();
             buttons = new List<Button>();
+            initalizeButtons();
+            setTextForButtons();
+
+        }
+
+        private void initalizeButtons()
+        {
+            buttons.Add(this.button1);
+            buttons.Add(this.button2);
+            buttons.Add(this.button3);
+            buttons.Add(this.button4);
+            buttons.Add(this.button5);
+            buttons.Add(this.button6);
+        }
+
+        private void setTextForButtons()
+        {
+            Random rng = new Random();
+            int next;
+            //3*2 = 6
+            Dictionary<string, string> keyValuePairs = hiragana.getRandomChars(3);
+            //all of this only works because the amount of buttons has to be even
+            do
+            {
+                var keyValuePair = keyValuePairs.First();
+                next = rng.Next(buttons.Count);
+                buttons[next].Text = keyValuePair.Key;
+                buttons.RemoveAt(next);
+                next = rng.Next(buttons.Count);
+                buttons[next].Text = keyValuePair.Value;
+                buttons.RemoveAt(next);
+                keyValuePairs.Remove(keyValuePair.Key);
+            } while (buttons.Count > 0 && keyValuePairs.Count > 0);
         }
 
         private void Button1_Click(object sender, EventArgs e)
